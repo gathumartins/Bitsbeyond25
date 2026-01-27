c90dadbe4e876c11474f23b6e543e410d770805d
gathumartins <gathumartins@gmail.com>
2026-01-27 15:33:20 +0300

    fixed home page overflow issue by fixing the bug on testimonial slider

--- src/app/components/TestimonalsSlider.tsx
+++ src/app/components/TestimonalsSlider.tsx
@@ -11,21 +11,20 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function TestimonalsSlider() {
   return (
-    <section className="min-h-[250px] bg-white dark:bg-white contPadd py-11">
+    <section className="min-h-[250px] bg-white dark:bg-white contPadd py-11 overflow-hidden">
       <h2 className="bodyH2 mb-5">Testimonials</h2>
-      <Carousel opts={{ align: "start", loop: true }} className='w-full overflow-hidden'>
+      <Carousel opts={{ align: "start", loop: true }} className="">
         <CarouselContent className="-mr-60 md:-mr-40 flex items-stretch">
-          {[1,2,3,4,5,6,7,8,9].map((test: any, index: number) => (
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((test: any, index: number) => (
             <CarouselItem
               className="basis-1/2 md:basis-1/3 lg:basis-1/4 pl-2 md:pl-4"
               key={index}
             >
               <Card className="pt-[20px] bg-accent dark:bg-accent border-0 flex flex-col justify-between h-full rounded">
                 <CardContent>
-                  <div
-                    className="text-base font-sans grow"
-                  >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta vero, iste illo voluptate odit beatae!
+                  <div className="text-base font-sans grow">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dicta vero, iste illo voluptate odit beatae!
                   </div>
                 </CardContent>
                 <CardFooter className="flex flex-row gap-5">
